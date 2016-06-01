1deb2def437644931d52d0f918190264e9ef5d13
inna185 <innawow@mail.ru>
2016-06-02 01:17:59 +0300

    Нормализация входных данных для нейросети

---
+++ app/src/main/java/com/example/kursach/neural/DataNormalization.java
@@ -0,0 +1,23 @@
+package com.example.kursach.neural;
+
+/**
+ * Created by 1 on 02.06.2016.
+ */
+public class DataNormalization {
+
+    public static double normalizeMassIndex(double index){
+        return (index-16)/24;
+    }
+
+    public static double normalizePeriod(double period){
+        return (period-1)/7;
+    }
+
+    public static double normalizeWeight(double weight){
+        return (weight-2)/18;
+    }
+
+    public static double getMassIndex(int weight, int growth){
+        return weight/(growth*growth/10000);
+    }
+}
