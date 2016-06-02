5a2ec3ed8def59f62ebd06b9ac6480538b6b9e69
DMITRY <manujlv@mail.ru>
2016-06-03 01:17:23 +0300

    ***

--- app/src/main/java/com/example/kursach/MainPage.java
+++ app/src/main/java/com/example/kursach/MainPage.java
@@ -2,12 +2,18 @@ package com.example.kursach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
